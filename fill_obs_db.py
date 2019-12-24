55a6b5306c8482aeeabe4201dc4474f978e60e54
Jeremy Cushman <jscushman@gmail.com>
2019-12-23 20:40:43 -0600

    PHP along with scripts to fill sqlite dbs

---
+++ fill_obs_db.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sqlite3
+import argparse
+import csv
+
+def import_observations(file_name, user_id):
+    with open(file_name, mode='r') as infile:
+        reader = csv.DictReader(infile)
+        with sqlite3.connect("ebird_obs_data.sqlite") as conn:
+            cur = conn.cursor()
+    
+            cur.execute("DELETE FROM Observations")
+            observations_insert_sql = "INSERT INTO Observations(submission_id, common_name, scientific_name, state_id, county_name, date, user) VALUES(?,?,?,?,?,?,?)"
+            for row in reader:
+                cur.execute(observations_insert_sql, (row["Submission ID"], row["Common Name"], row["Scientific Name"], row["State/Province"], row["County"], row["Date"], user_id))
+
+if __name__== "__main__":
+    parser = argparse.ArgumentParser(description='Import observation data.')
+    parser.add_argument('file_name', type=str, help='csv file name')
+    parser.add_argument('user_id', type=str, help='user ID')
+    args = parser.parse_args()
+    import_observations(args.file_name, args.user_id)
---
+++ obs_search.php
@@ -0,0 +1,23 @@
+<?php
+$county = $_GET['county'];
+if (isset($county)) {
+  $region_db = new SQLite3('db/ebird_region_data.sqlite');
+  $region_query = "SELECT name FROM Counties WHERE code = \"$county\"";
+  $res = $region_db->query($region_query);
+  $county_name_row = $res->fetchArray();
+  $county_name = $county_name_row['name'];
+}
+$obs_db = new SQLite3('db/ebird_obs_data.sqlite');
+$state = $_GET['state'];
+$query = "SELECT DISTINCT common_name FROM Observations WHERE state_id = \"$state\"";
+if (isset($county)) {
+  $query .= " AND county_name = \"$county_name\"";
+}
+$res = $obs_db->query($query);
+$species = array();
+
+while ($row = $res->fetchArray()) {
+  array_push($species, $row['common_name']);
+}
+echo json_encode($species);
+?>
