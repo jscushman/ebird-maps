e09b29c99466a7baafafc857bea173316b37eb3a
Jeremy Cushman <jscushman@gmail.com>
2020-02-17 00:12:58 -0500

    Wait for map loading

--- ebird_map.js
+++ ebird_map.js
@@ -1,97 +1,12 @@
 mapboxgl.accessToken = "***REMOVED***";
 
-var map;
-document.addEventListener('DOMContentLoaded', function() {
-  // Map to show all observations
-   map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/light-v9",
-    center: [0, 0],
-    zoom: 8
-  });
-
-  map.on("load", function() {
-    map.addSource("ebird-sightings", {"type": "geojson", "data": {"type": "FeatureCollection", "features": []}});
-    map.addLayer({
-      "id": "points",
-      "type": "circle",
-      "source": "ebird-sightings",
-      "paint": {
-        "circle-opacity" : {
-          "base": 0.0,
-          "stops": [[8, 0.5], [12, 0.5], [13, 1.0]]
-        },
-        "circle-radius": {
-          "base": 2,
-          "stops": [[8, 2], [11, 6], [22, 180]]
-        },
-        "circle-color": [
-          "match",
-          ["get", "time_ago_category"],
-          "today", "green",
-          "yesterday", "orange",
-          "old", "red", "black"
-        ]
-      }
-    });
-
-    map.addLayer({
-      "id": "points-labels",
-      "type": "symbol",
-      "source": "ebird-sightings",
-      "layout": {
-        "icon-allow-overlap": true,
-        "icon-image": "circle-stroked-11",
-        "text-field": "{title}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-        "text-offset": [0, 0.6],
-        "text-size": 12,
-        "text-anchor": "top",
-      },
-      "paint": {
-        "icon-color":"#9fcb3b"
-      }
-    });
-
-    // Set a popup to appear when a point is clicked.
-    map.on("click", "points", function (e) {
-      var coordinates = e.features[0].geometry.coordinates.slice();
-      var description = e.features[0].properties.description;
-      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-      }
-      new mapboxgl.Popup().setLngLat(coordinates).setHTML(description).addTo(map);
-    });
-  });
-}, false);
-
-function jump_map(lon, lat) {
-  map.jumpTo({center: [lon, lat]});
-}
-
-function add_map_source(name, spec) {
-  map.addSource(name, spec);
-}
-
-function add_map_layer(spec) {
-  map.addLayer(spec);
-}
-
-function get_map_center() {
-  return map.getCenter();
-}
-
-function set_source_data(name, data) {
-  map.getSource(name).setData(data);
-}
-
 var current_data_points = [];
 function set_data_points(data_points) {
   current_data_points = data_points;
 }
 
 // Plot data points on map.
-function update_map() {
+function update_map(map) {
   console.log("Populating map with " + current_data_points.length + " points");
   let features = [];
   let now = moment();
@@ -142,7 +57,13 @@ function update_map() {
           description = description + "by " + obs.userDisplayName + (obs.obsReviewed ? "" : " (UNCONFIRMED)") + (obs.hasRichMedia ? " (with photo)" : "");
         }
         description = description + "<br>";
-        species_seen.add(obs.comName);
+        let photo_suffix = (obs.hasRichMedia ? " (P)" : "")
+        if (!species_seen.has(obs.comName) && !species_seen.has(obs.comName + " (P)")) {
+          species_seen.add(obs.comName + photo_suffix);
+        } else if (!species_seen.has(obs.comName + " (P)") && obs.hasRichMedia) {
+          species_seen.delete(obs.comName);
+          species_seen.add(obs.comName + photo_suffix);
+        }
       });
       // The title that is displayed on the map is a (de-duplicated) list of species.
       let title = [...species_seen].join(",\n");
@@ -161,5 +82,5 @@ function update_map() {
       features.push(new_feature);
     }
   });
-  set_source_data("ebird-sightings", {"type": "FeatureCollection", "features": features});
+  map.getSource("ebird-sightings").setData({"type": "FeatureCollection", "features": features});
 }
