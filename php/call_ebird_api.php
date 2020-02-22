43c65618bed564dcfac2cb229d888f6cb2beefbe
Jeremy Cushman <jscushman@gmail.com>
2020-02-22 14:41:18 -0500

    Move API call to php wrapper

---
+++ php/call_ebird_api.php
@@ -0,0 +1,29 @@
+<?php
+header('Content-type:application/json;charset=utf-8');
+
+$data = array(
+    'lat' => $_GET['lat'],
+    'lng' => $_GET['lng'],
+    'back' => $_GET['back'],
+    'dist' => $_GET["dist"]
+);
+
+$curl = curl_init();
+$url = sprintf("https://api.ebird.org/v2/data/obs/geo/recent/notable?%s", http_build_query($data));
+
+$api_file = fopen(".ebird_api_token", "r");
+if (!feof($api_file)) {
+  $header = fgets($api_file);
+} else {
+  $header = '';
+}
+fclose($api_file);
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array($header));
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+echo $result;
