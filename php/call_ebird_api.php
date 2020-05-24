1c696a263c79a9ff32e59c3154bf96d8986a7ca9
Jeremy Cushman <jscushman@gmail.com>
2020-05-24 15:52:00 -0400

    Update php script

--- php/call_ebird_api.php
+++ php/call_ebird_api.php
@@ -17,12 +17,16 @@ if (!feof($api_file)) {
 fclose($api_file);
 
 $curl = curl_init();
+
 $url = sprintf("https://api.ebird.org/v2/data/obs/geo/recent/notable?sppLocale=en&detail=full&%s", http_build_query($data));
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array($header));
+curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 $result = curl_exec($curl);
-
+if($result === false) {
+  echo 'Curl error: ' . curl_error($curl);
+} else {
+  echo $result;
+}
 curl_close($curl);
-
-echo $result;
